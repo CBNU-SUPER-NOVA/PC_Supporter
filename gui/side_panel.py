d720cfd63d5a7f44ba0d445ca4991b0d0b9cab8d
SeoBeomSu <beom710@naver.com>
2024-08-15 22:27:26 +0900

    Modify : side_panel add button

--- gui/side_panel.py
+++ gui/side_panel.py
@@ -1,14 +1,20 @@
 import wx
+import wx.svg
+from componets.SVGButton import SVGButton
 
 
 class OverlayPanel(wx.Panel):
     def __init__(self, parent):
         super(OverlayPanel, self).__init__(
-            parent, pos=(50, 50), size=(200, 150))
+            parent, pos=(50, 50), size=(800, 800))
         self.SetBackgroundColour("light blue")
 
+        # SVG 이미지 렌더링 패널 생성
+        self.svg_panel = SVGButton(
+            self, "gui/icons/SideBar.svg", 40, self.on_close_button_click)
+
         # 닫기 버튼 생성
-        self.close_button = wx.Button(self, label="Close", pos=(50, 50))
+        self.close_button = wx.Button(self, label="Close", pos=(50, 80))
 
         # 버튼 클릭 시 패널을 숨기도록 이벤트 핸들러 바인딩
         self.close_button.Bind(wx.EVT_BUTTON, self.on_close_button_click)
