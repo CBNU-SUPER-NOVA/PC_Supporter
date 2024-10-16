80c3cf5975ee902817e231188465c2989bf3374e
beom710 <beom710@naver.com>
2024-10-16 23:49:55 +0900

    [Feat] : Workflow 실행 알림

--- gui/code_panel.py
+++ gui/code_panel.py
@@ -32,7 +32,7 @@ class CodePanel(wx.Panel):
         # 독립적인 버튼 생성
         WorkflowRunButton = RoundedPanel(
             self, size=(300, 50), radius=25, alignment="center", texts="Workflow Run", color="#F7F7F8", hover_color="#C0C0C0")
-        
+
         WorkflowRunButton.Bind(wx.EVT_LEFT_DOWN, self.workflowRun)  # 이벤트 핸들러로 수정
         main_sizer.Add(WorkflowRunButton, 0, wx.ALIGN_CENTER | wx.ALL, 10)
 
@@ -116,7 +116,7 @@ class CodePanel(wx.Panel):
         # 동일한 유형의 코드 블록을 그룹화하여 저장할 변수들
         combined_code = []
         current_type = None
-        
+
         # self.code_blocks 리스트의 코드 블록을 order_num 순서대로 실행
         for code_block in sorted(self.code_blocks, key=lambda x: x[3]):  # order_num을 기준으로 정렬
             code_id, code_type, code_data, order_num = code_block
@@ -144,7 +144,9 @@ class CodePanel(wx.Panel):
             output = execute_code(code_to_execute, current_type)
             print(output)
 
-    
+        # 코드가 실행되었음을 알림 (실행 결과를 어떻게 처리할지는 나중에 결정)
+        wx.MessageBox("Workflow Run 완료", "알림", wx.OK | wx.ICON_INFORMATION)
+
     def OnDragStart(self, event):
         self.dragging_item = event.GetEventObject().GetParent()
         self.dragging_item.Hide()
